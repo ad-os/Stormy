7fc7ff1640a2b43b365543ae4c3adc49cd62d5e1
Ad <adhyan_srivastava95@yahoo.com>
2015-06-23 14:01:55 +0530

    Added a asynchronous http request.

--- app/src/main/java/com/example/android/stormy/MainActivity.java
+++ app/src/main/java/com/example/android/stormy/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
@@ -37,14 +38,27 @@ public class MainActivity extends ActionBarActivity {
 
         Call call = client.newCall(request);
 
-        try {
-            Response response = call.execute();
-            if(response.isSuccessful()){
-                Log.v(TAG, response.body().string());
+
+        call.enqueue(new Callback() {
+
+            @Override
+            public void onFailure(Request request, IOException e) {
+                Log.e(TAG, "Exception caught : ", e);
             }
-        } catch (IOException e) {
-            Log.e(TAG, "Exception caught : ", e);
-        }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                try {
+                    if(response.isSuccessful()){
+                        Log.v(TAG, response.body().string());
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Exception caught : ", e);
+                }
+
+            }
+        });
+
     }
 
 }
