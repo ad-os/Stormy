1f910e8b7c19e643792c1eeeceb834d1169bd48e
Ad <adhyan_srivastava95@yahoo.com>
2015-06-24 02:27:05 +0530

    Added a network check function.

--- app/src/main/java/com/example/android/stormy/MainActivity.java
+++ app/src/main/java/com/example/android/stormy/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.android.stormy;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
@@ -31,35 +33,52 @@ public class MainActivity extends ActionBarActivity {
         String forecastUrl = "https://api.forecast.io/forecast/" + apiKey +
                 "/" + latitude + "," + longitude;
 
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().
-                url(forecastUrl).
-                build();
+        if(isNetworkConnected()) {
 
-        Call call = client.newCall(request);
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().
+                    url(forecastUrl).
+                    build();
+
+            Call call = client.newCall(request);
 
 
-        call.enqueue(new Callback() {
+            call.enqueue(new Callback() {
 
-            @Override
-            public void onFailure(Request request, IOException e) {
-                Log.e(TAG, "Exception caught : ", e);
-            }
-
-            @Override
-            public void onResponse(Response response) throws IOException {
-                try {
-                    Log.v(TAG, response.body().string());
-                    if(response.isSuccessful()){
-                    }else {
-                        alertUserAboutError();
-                    }
-                } catch (IOException e) {
+                @Override
+                public void onFailure(Request request, IOException e) {
                     Log.e(TAG, "Exception caught : ", e);
                 }
 
-            }
-        });
+                @Override
+                public void onResponse(Response response) throws IOException {
+                    try {
+                        Log.v(TAG, response.body().string());
+                        if (response.isSuccessful()) {
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught : ", e);
+                    }
+
+                }
+            });
+        }else {
+            Toast.makeText(this, "No network available", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean isNetworkConnected() {
+        //Context contains the information about the application resources and classes.
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        boolean isConnected = false;
+        if (networkInfo != null && networkInfo.isConnected()){
+            isConnected = true;
+        }
+        return isConnected;
     }
 
     private void alertUserAboutError() {
