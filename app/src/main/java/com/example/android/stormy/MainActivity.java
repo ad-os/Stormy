4ec3b4b1285a9c671047880b5010b4568ed520d6
Ad <adhyan_srivastava95@yahoo.com>
2015-06-24 05:48:21 +0530

    Collected data from JSON

--- app/src/main/java/com/example/android/stormy/MainActivity.java
+++ app/src/main/java/com/example/android/stormy/MainActivity.java
@@ -14,12 +14,17 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+
+    private CurrentWeather mCurrentWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,20 +58,44 @@ public class MainActivity extends ActionBarActivity {
                 @Override
                 public void onResponse(Response response) throws IOException {
                     try {
-                        Log.v(TAG, response.body().string());
+                        String jsonData = response.body().string();
+                        Log.v(TAG, jsonData);
                         if (response.isSuccessful()) {
+                            mCurrentWeather = getCurrentDetails(jsonData);
                         } else {
                             alertUserAboutError();
                         }
                     } catch (IOException e) {
                         Log.e(TAG, "Exception caught : ", e);
+                    } catch (JSONException e){
+                        Log.e(TAG, "Exception caught : ", e);
                     }
-
                 }
             });
         }else {
             Toast.makeText(this, "No network available", Toast.LENGTH_LONG).show();
         }
+
+        Log.v(TAG, "Executing main thread");
+    }
+
+    //here with throws key word we are passing the responsibility to whoever calls this method.
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
+        JSONObject forecast = new JSONObject(jsonData);
+        String timezone = forecast.getString("timezone");
+        JSONObject currently = forecast.getJSONObject("currently");
+
+        CurrentWeather currentWeather = new CurrentWeather();
+
+        currentWeather.setIcon(currently.getString("icon"));
+        currentWeather.setHumidity(currently.getDouble("humidity"));
+        currentWeather.setPrecipChance(currently.getInt("precipProbability"));
+        currentWeather.setSummary(currently.getString("summary"));
+        currentWeather.setTemperature(currently.getDouble("temperature"));
+        currentWeather.setTime(currently.getLong("time"));
+        Log.i(TAG, "From Json" + timezone);
+
+        return currentWeather;
     }
 
     private boolean isNetworkConnected() {
@@ -75,6 +104,8 @@ public class MainActivity extends ActionBarActivity {
                 (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
         boolean isConnected = false;
+        //networkInfo != null to check if network is available
+        //networkInfo.isConnected() to check if network is available.
         if (networkInfo != null && networkInfo.isConnected()){
             isConnected = true;
         }
